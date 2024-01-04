399ad80a598bb590e161d49885cc87d69605890a
Akane <115964926+redzx69@users.noreply.github.com>
2024-01-04 20:04:17 +0800

    Delete Project directory
    moving this to a new repo for itself

--- Project/src/ClinicAppQueue.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class ClinicAppQueue {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        Scanner scan1 = new Scanner(System.in);
-        Queue patientQueue = new Queue();
-    }
-}
